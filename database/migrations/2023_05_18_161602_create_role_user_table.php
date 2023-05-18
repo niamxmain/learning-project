aa57ceb3aa9f174012d5fabbaab44dbe9adef91a
niamx <muhammadniam1999@gmail.com>
2023-05-18 23:43:18 +0700

    add pivot table role_user

--- database/migrations/2023_05_18_161602_create_role_user_table.php
+++ database/migrations/2023_05_18_161602_create_role_user_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('role_id')->constrained();
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role_user');
     }
 };

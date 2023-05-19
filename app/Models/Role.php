2181785cfb0a551d7027ccd29ac2d1b88d80cc64
niamx <muhammadniam1999@gmail.com>
2023-05-19 22:36:19 +0700

    update role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,10 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
     use HasFactory;
-    
+
+    /**
+     * The users that belong to the role.
+     */
+    public function users()
+    {
+        // return $this->belongsToMany(User::class)->withTimestamps();
+        return $this->belongsToMany(User::class);
+    }
 }
--- database/migrations/2023_05_18_161602_create_role_user_table.php
+++ database/migrations/2023_05_18_161602_create_role_user_table.php
@@ -12,10 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('role_id')->constrained();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->primary(['user_id', 'role_id']);
         });
     }
 

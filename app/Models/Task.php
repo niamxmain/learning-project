f7f1c6b34bbd9dec0bec3f693acac7df9b613a0d
niamx <muhammadniam1999@gmail.com>
2023-05-24 22:50:37 +0700

    add relations

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function subtasks()
+    {
+        return $this->hasMany(Task::class, 'parent_id');
+    }
+
+    public function parentTask()
+    {
+        return $this->belongsTo(Task::class, 'parent_id');
+    }
 }

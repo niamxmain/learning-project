f3251fb97ad0f59e3d4c8fff868703bbc811992d
niamx <muhammadniam1999@gmail.com>
2023-05-30 23:50:47 +0700

    feat: add route

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/data', App\Http\Controllers\Api\ProjectController::class);
+Route::apiResource('/projects', App\Http\Controllers\Api\ProjectController::class);
 Route::apiResource('/post', App\Http\Controllers\Api\ProjectController::class);
+Route::apiResource('/project', App\Http\Controllers\Api\ProjectController::class);
+
